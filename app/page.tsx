8089bf4931cd84b8b95ed9bffe427e5bbdf9f8f9
KaliMerO666 <kalimero@ik.me>
2023-09-06 13:48:36 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,31 +31,31 @@ export default function Home() {
 				className="absolute inset-0 -z-10 animate-fade-in"
 				quantity={100}
 			/>
-			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				chronark
+			<h1 className="z-10 text-3xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-5xl md:text-8xl whitespace-nowrap bg-clip-text ">
+				www.kalimero.rocks
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
+					Hi, my name is Kalimero, I'm a Wild at Art
+					 {" "}
 					<Link
 						target="_blank"
-						href="https://upstash.com"
+						href="https://yamwhatyam.kalimero.rocks"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						Upstash
+						Dessine-Man
 					</Link>
 
 					<br />
-					and working on{" "}
+					working on{" "}
 					<Link
 						target="_blank"
-						href="https://unkey.dev"
+						href="https://nest-in-pace.kalimero.rocks"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						unkey.dev
+						graphic novels
 					</Link>{" "}
 					at night.
 				</h2>
